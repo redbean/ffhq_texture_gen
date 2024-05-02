3d96a9bc380dc1f76098a3669aff07ad5212977a
Jeewon Park <redbeanmouse@gmail.com>
2024-05-02 17:47:20 +0900

    데이터 npy 제작

---
+++ data_const.py
@@ -0,0 +1,17 @@
+class STRINGS:
+    DIR               = "ffhq-uv-intrinsics-"
+    OUTPUT = "Data_preprocessed"
+    FILES = ['ambient_occlusion.png',
+            'diffuse.png',
+            'light_normalized.png',
+            'normal.png',
+            'specular.png',
+            'translucency.png']
+    FILE_AO           = 0
+    FILE_DIFF         = 1
+    FILE_LN           = 2
+    FILE_NORM         = 3
+    FILE_SPEC         = 4
+    FILE_TRANSLUCENCY = 5
+    
+    
