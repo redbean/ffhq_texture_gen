980b1920dc11fe6a231ef81f2870e94e5558053a
Jeewon Park <redbeanmouse@gmail.com>
2024-06-05 00:49:47 +0900

    4,3,wh에서 12,wh 로 변경

--- codec/MyDecoder.py
+++ codec/MyDecoder.py
@@ -26,18 +26,12 @@ class UNetBlock(nn.Module):
         x = x + res  # skip connection 추가
         return x
 
-
 class MyDecoder(nn.Module):
     def __init__(self, input_dim=256):
         super(MyDecoder, self).__init__()
         self.fc = nn.Linear(input_dim, 512 * 4 * 4)
 
-        self.diffuse_up_layers = self._create_up_layers()
-        self.light_normalized_up_layers = self._create_up_layers()
-        self.normal_up_layers = self._create_up_layers()
-        self.specular_up_layer = self._create_gray_up_layer()
-        self.ao_up_layer = self._create_gray_up_layer()
-        self.translucency_up_layer = self._create_gray_up_layer()
+        self.up_layers = self._create_up_layers()
 
     def _create_up_layers(self):
         return nn.Sequential(
@@ -47,46 +41,15 @@ class MyDecoder(nn.Module):
             UNetBlock(256, 128, up=True),
             UNetBlock(128, 64, up=True),
             UNetBlock(64, 32, up=True),
-            nn.ConvTranspose2d(32, 3, 4, 2, 1),
-            nn.Tanh()
-        )
-
-    def _create_gray_up_layer(self):
-        return nn.Sequential(
-            UNetBlock(512, 512, up=True),
-            UNetBlock(512, 512, up=True),
-            UNetBlock(512, 256, up=True),
-            UNetBlock(256, 128, up=True),
-            UNetBlock(128, 64, up=True),
-            UNetBlock(64, 32, up=True),
-            nn.ConvTranspose2d(32, 1, 4, 2, 1),
+            nn.ConvTranspose2d(32, 12, 4, 2, 1),
             nn.Tanh()
         )
 
     def forward(self, x):
         x = self.fc(x)
         x = x.view(-1, 512, 4, 4)
-
-        diffuse = self.diffuse_up_layers(x)
-        light_normalized = self.light_normalized_up_layers(x)
-        normal = self.normal_up_layers(x)
-
-        specular = self.specular_up_layer(x)
-        ao = self.ao_up_layer(x)
-        translucency = self.translucency_up_layer(x)
-
-        return diffuse, light_normalized, normal, specular, ao, translucency
+        x = self.up_layers(x)
+        return x
 
     def decode(self, x):
-        batch_size = x.shape[0]
-        output_images = torch.zeros((batch_size, 4, 3, 512, 512), device=x.device)
-
-        diffuse, light_normalized, normal, specular, ao, translucency = self.forward(x)
-
-        output_images[:, 0, :, :, :] = diffuse
-        output_images[:, 1, :, :, :] = light_normalized
-        output_images[:, 2, :, :, :] = normal
-
-        merged_gray_image = torch.cat((specular, ao, translucency), dim=1)
-        output_images[:, 3, :, :, :] = merged_gray_image
-        return output_images
+        return self.forward(x)
